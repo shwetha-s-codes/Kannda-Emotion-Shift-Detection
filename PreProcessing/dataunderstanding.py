4de00a9aa2adbe9bd7bf6b6ea97ea205b955a25f
shwetha-s-codes <shwethakodimar29@gmail.com>
2025-06-19 18:28:11 +0530

    Preprocessing the data

---
+++ PreProcessing/dataunderstanding.py
@@ -0,0 +1,10 @@
+#python script to understand the data
+import pandas as pd
+
+df=pd.read_csv("dataset_1.csv")
+null_rows = df[df.isnull().any(axis=1)]
+print(null_rows)
+print(df.columns)
+print(df.head)
+print(df.isnull().sum())
+print(len(df))
